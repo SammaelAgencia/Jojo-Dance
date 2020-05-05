c1320659fc08bad2cfc3dd6fb0e1d57a32882bdb
Jpstdraws <juanpanblo24@hotmail.com>
2020-05-05 12:45:51 -0500

    Version 1

---
+++ jojo Dance/Assets/scripts/SoundBtn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundBtn : MonoBehaviour {
+
+    public AudioSource myFx;
+    public AudioClip hoverFx;
+    public AudioClip clickfx;
+
+    public void HoverSound() {
+        myFx.PlayOneShot(hoverFx);
+    }
+    public void ClickSound()
+    {
+        myFx.PlayOneShot(clickfx);
+    }
+	
+}
